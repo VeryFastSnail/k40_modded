7eebb2c6d78b25cee06208a74941e0f0dc17feea
Rokas Raulinaitis <rokasraulinaitis@hotmail.com>
2020-12-30 18:14:30 +0200

    Updated min size of application for usable size

--- k40_whisperer.py
+++ k40_whisperer.py
@@ -6003,8 +6003,8 @@ root = Tk()
 app = Application(root)
 app.master.title(title_text)
 app.master.iconname("K40")
-app.master.minsize(800, 560)
-app.master.geometry("800x560")
+app.master.minsize(800, 800)
+app.master.geometry("800x800")
 try:
     try:
         import tkFont
